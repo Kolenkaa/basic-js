75eb787652580e0b490a0eded48360df63ed5654
Kolenkaa <kkolenkaa@gmail.com>
2022-04-29 14:41:51 +0300

    fix:change check for what-saeson.js

--- src/what-season.js
+++ src/what-season.js
@@ -15,14 +15,14 @@ function getSeason(date) {
 	if (!arguments.length) {
 		return "Unable to determine the time of year!";
 	}
+	let result;
 	try {
-		let result = date.getMonth();
+		date.toJSON();
+		result = date.getMonth();
 	} catch (error) {
 		throw new Error("Invalid date!")
 	}
-	if (date[Symbol.toStringTag] === "Date") throw new Error("Invalid date!");
 
-	let result = date.getMonth();
 	if (result === 2 || result === 3 || result === 4) {
 		return "spring";
 	} else if (result === 5 || result === 6 || result === 7) {
@@ -31,9 +31,7 @@ function getSeason(date) {
 		return "autumn";
 	} else if (result === 11 || result === 0 || result === 1) {
 		return "winter";
-	} throw new Error("Invalid date!");
-
-	return result;
+	} else { throw new Error("Invalid date!"); }
 }
 
 module.exports = {
