f4bb2ae92c7366292639eb2966de00ec8a07a8e2
Kolenkaa <kkolenkaa@gmail.com>
2022-05-02 11:29:31 +0300

    feat: add solution  Common-character-count.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+	if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+
+	const transformedArr = [];
+
+	const DOUBLE_PREV = "--double-prev";
+	const DOUBLE_NEXT = "--double-next";
+	const DISCARD_PREV = "--discard-prev";
+	const DISCARD_NEXT = "--discard-next";
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] !== DOUBLE_PREV && arr[i] !== DOUBLE_NEXT && arr[i] !== DISCARD_PREV && arr[i] !== DISCARD_NEXT) {
+			return arr;
+		}
+		if (arr[i] === DOUBLE_PREV) {
+			transformedArr = [
+				...arr.slice(0, i),
+				arr[i - 1],
+				...arr.slice(i + 1)
+			];
+		}
+		if (arr[i] === DOUBLE_NEXT) {
+			transformedArr = [
+				...arr.slice(0, i),
+				arr[i + 1],
+				...arr.slice(i + 1)
+			];
+		}
+		if (arr[i] === DISCARD_PREV) {
+			transformedArr = [...arr.slice(0, i - 1), ...arr.slice(i + 1)];
+		}
+		if (arr[i] === DISCARD_NEXT) {
+			transformedArr = [
+				...arr.slice(0, i),
+				arr[i - 1],
+				...arr.slice(i + 2)
+			];
+		}
+	}
+
 }
 
 module.exports = {
-  transform
+	transform
 };
